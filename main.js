891831cedb98afd80f9b164500f6f34d2be95b8a
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-10-25 07:23:14 +0300

    final

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 /**
  * Created by User on 25.10.2016.
  */
+var admin;
+var user;
+
+user = "Asya";
+admin = user;
+
+console.log(user);
+
+var login = prompt('Please, enter your login');
+
+if (login == "admin") {
+    pass = prompt('Please, enter your password');
+
+
+
+
+} else if ( login == null ) {
+    alert( 'enter cancel');
+} else  {
+    alert( 'Ohh... i dont know you ');
+}
+
+
+if (pass != "1111") {
+    alert ('ohhh... it is not right answer');
+
+} else {
+    alert ('hii!');
+}
+
+
+
+
+
